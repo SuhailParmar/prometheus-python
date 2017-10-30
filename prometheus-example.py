9e357ad60b8d15ed5a548ee2ac049c2063858110
Suhail Parmar <sparmar@skyscapecloud.com>
2017-10-30 14:30:53 +0000

    Updated Readme!

--- prometheus-example.py
+++ prometheus-example.py
@@ -21,7 +21,7 @@ def process_request(t):
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
-    print 'Started Server on Port 8080'
+    print Started Server on Port 8080
     # Generate some requests.
     while True:
         process_request(random.random())
